2a1cf4089fa33188ef611c151d0802f1900695fa
leizuquan <843291011@qq.com>
2020-03-09 11:30:03 +0800

    # 调试java版sparkSQL rdd与dataframe、dataset相互之间转换

--- src/main/java/com/java/spark_sql/Java_SparkSQL03_transform.java
+++ src/main/java/com/java/spark_sql/Java_SparkSQL03_transform.java
@@ -1,14 +1,14 @@
 package com.java.spark_sql;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.*;
-import scala.Tuple2;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -20,6 +20,8 @@ import java.util.List;
  * @Description: sparkSQL RDD、DataFrame、DataSet相互之间转换
  */
 public class Java_SparkSQL03_transform {
+    //private static Object User;
+
     public static void main(String[] args) {
         // 1.初始化spark配置信息并建立与spark的连接
         SparkConf config = new SparkConf().setMaster("local[*]").setAppName("SparkSQL03_transform");
@@ -31,11 +33,11 @@ public class Java_SparkSQL03_transform {
 
         // 创建RDD
         // var rdd: RDD[(Int, String, Int)] = session.sparkContext.makeRDD(List((1, "zhagnshan", 10), (2, "lisi", 20)))
-        List<Tuple2<Integer, String>> list = new ArrayList<>();
-        list.add(new Tuple2<>(1, "a"));
-        list.add(new Tuple2<>(2, "b"));
-        list.add(new Tuple2<>(3, "c"));
-        JavaPairRDD<Integer, String> rdd = sc.parallelizePairs(list, 2);
+        List<User> list = new ArrayList<>();
+        list.add(new User(1, "zhangsan", 10));
+        list.add(new User(2, "lisi", 20));
+        list.add(new User(3, "wangwu", 30));
+        JavaRDD<User> rdd = sc.parallelize(list, 2);
 
         // 进行转换前，需要引入隐式转换规则。
         // 这里spark_session不是包的名字，是SparkSession的对象
@@ -43,43 +45,70 @@ public class Java_SparkSQL03_transform {
 
         // 转换为DF
         //var df:DataFrame = rdd.toDF("id", "name", "age")
-        // Encoders are created for Java beans
-        People people = new People();
-        people.setName("Andy");
-        people.setId(1);
+        JavaRDD<Row> studentRDD = rdd.map(user -> {
+            return RowFactory.create(user.getId(), user.getName(), user.getAge());
+        });
+        // 第二步，动态构造元数据
+        // 比如说，id、name等，field的名称和类型，可能都是在程序运行过程中，动态从mysql db里
+        // 或者是配置文件中，加载出来的，是不固定的
+        // 所以特别适合用这种编程的方式，来构造元数据
+        List<StructField> structFields = new ArrayList<StructField>();
+        structFields.add(DataTypes.createStructField("id", DataTypes.IntegerType, true));
+        structFields.add(DataTypes.createStructField("name", DataTypes.StringType, true));
+        structFields.add(DataTypes.createStructField("age", DataTypes.IntegerType, true));
+        StructType structType = DataTypes.createStructType(structFields);
 
-        Encoder<People> personEncoder = Encoders.bean(People.class);
-        // Dataset<Row> dataFrame = session.createDataFrame(list, People.class);
-        Dataset<Row> df = session.createDataFrame(Collections.singletonList(people), People.class);
 
+        // 第三步，将使用动态构造的元数据，将RDD转换为DataFrame
+        /**
+         * 特别说明：在java版本中没有DataFrame类型，代替的是： Dataset<Row>
+         *     在spark 2.x版本中 认为dataframe是 Dataset的形式：Dataset<Row>
+         *
+         * 如果同样的数据都给到这三个数据结构，他们分别计算之后，都会给出相同的结果。不同是的他们的执行效率和执行方式。
+         * 在后期的Spark版本中，DataSet会逐步取代RDD和DataFrame成为唯一的API接口。
+         */
+        // 转换为DF
+        // var df: DataFrame = rdd.toDF("id", "name", "age")
+        // 创建方式一：RDD (Spark1.0) —> Dataframe(Spark1.3) —> Dataset(Spark1.6)
+        Dataset<Row> old1_6_df = session.createDataFrame(studentRDD, structType);
+        old1_6_df.show();
+
+        // 创建方式二：推荐使用这种方式
+        Dataset<Row> df = session.createDataFrame(list, User.class);
         df.show();
 
         // 转换为DS
         // var ds: Dataset[User] = df.as[User] //这里需要创建样例类
-        Encoder<People> peopleEncoder = Encoders.bean(People.class);
-        Dataset<People> ds = df.as(peopleEncoder);
+
+        Encoder<User> userEncoder = Encoders.bean(User.class);
+        Dataset<User> ds = df.as(userEncoder);
+        System.out.println("=============================================");
+        ds.show();
+        System.out.println("=============================================");
+
         // 转换为DF
         // var df1:DataFrame = ds.toDF()
         Dataset<Row> df1 = ds.toDF();
+
         // 转换为RDD
-        //var rdd1:RDD[Row] = df1.rdd //这里是 row 类型需要注意
-        RDD<Row> rdd1 = df1.rdd();
-        Row[] rows = rdd1.collect();
-        for (Row row : rows) {
+        /*
+        var rdd1:RDD[Row] = df1.rdd //这里是 row 类型需要注意
+        rdd1.foreach(row -> {
+            // 获取数据时，可以通过索引访问数据
             System.out.print(row.getInt(0) + "\t");
             System.out.print(row.getString(1) + "\t");
-        }
-
-//        rdd1.foreach(row -> {
-//            // 获取数据时，可以通过索引访问数据
-//            System.out.print(row.getInt(0) + "\t");
-//            System.out.print(row.getString(1) + "\t");
-//            // System.out.println(row.getInt(2) + "\t");
-//            return null;
-//        });
+            // System.out.println(row.getInt(2) + "\t");
+            return null;
+        }); */
+        JavaRDD<Row> rdd1 = df1.javaRDD();
+        rdd1.foreach( row -> {
+            String userInfo = row.getInt(0) + "\t" + row.getInt(1) + "\t" + row.getString(2) + "\t";
+            System.out.println(userInfo);
+        });
 
         // 释放资源
         session.stop();
     }
 }
+
 
---
+++ src/main/java/com/java/spark_sql/User.java
@@ -0,0 +1,43 @@
+package com.java.spark_sql;
+
+import java.io.Serializable;
+
+//创建样例类，DataSet需要类型
+public class User implements Serializable {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public User(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
